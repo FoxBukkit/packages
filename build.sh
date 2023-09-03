a22263c41f25456186c96f315e407028cfa92046
Doridian <git@doridian.net>
2023-09-02 23:06:54 -0700

    auth

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ for repo in `cat ../maven-repos.txt`
 do
     git clone "https://github.com/FoxBukkit/$repo"
     pushd "$repo"
-    mvn --batch-mode --update-snapshots clean package deploy
+    mvn --batch-mode --update-snapshots clean package deploy -s "$GITHUB_WORKSPACE/settings.xml"
     popd
 done
