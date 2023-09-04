2140ff12709a4aa94beb202c8f9ad8ca8a330369
Doridian <git@doridian.net>
2023-09-04 16:35:35 -0700

    fix

--- docker/update.sh
+++ docker/update.sh
@@ -3,6 +3,3 @@ set -ex
 cd "$1"
 
 npm start --prefix /tools/updater
-
-wget https://dev.bukkit.org/projects/worldedit/files/latest -O worldedit.jar
-wget https://dev.bukkit.org/projects/worldguard/files/latest -O worldguard.jar
