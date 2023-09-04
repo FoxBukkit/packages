18dc02908ba092ca54dd15eadb28011f0770addf
Doridian <git@doridian.net>
2023-09-04 15:15:37 -0700

    these

--- docker/update.sh
+++ docker/update.sh
@@ -2,18 +2,7 @@
 set -ex
 cd "$1"
 
-update_package() {
-	node /tools/maven-downloader/index.js "$1" "$1.jar"
-}
-
-node /tools/maven-downloader/index.js foxbukkit-permissions foxbukkit-chat foxbukkit-lua
-
-if [ -d FoxBukkitLua/modules ]
-then
-    git -C FoxBukkitLua/modules pull
-else
-    git clone https://github.com/FoxBukkit/foxbukkit-lua-modules FoxBukkitLua/modules
-fi
+npm start --prefix /tools/updater
 
 wget https://dev.bukkit.org/projects/worldedit/files/latest -O worldedit.jar
 wget https://dev.bukkit.org/projects/worldguard/files/latest -O worldguard.jar
